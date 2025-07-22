c1f51f5709b34f5f936010547e589d2daefbcf6f
Winston Hoy <winston@curiousercreative.com>
2025-07-22 14:01:50 -0400

    update for example app

--- example/src/index.js
+++ example/src/index.js
@@ -1,7 +1,7 @@
 import './index.css'
 
 import React from 'react'
-import ReactDOM from 'react-dom'
+import * as ReactDOM from 'react-dom/client'
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
